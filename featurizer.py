ab584f1f15af8e84ed94f7e8b4b1aa34900939e6
KunalSinha1125 <ksinha2@stanford.edu>
2022-11-17 20:00:01 -0800

    Process other input types in featurizer.py

--- featurizer.py
+++ featurizer.py
@@ -5,6 +5,9 @@ import spacy
 
 spacy_nlp = spacy.load("en_core_web_sm")
 GLOVE_FILE = "glove.6B.50d.txt"
+EXTRA_FILE = "OYA_AcademicJobs.csv"
+RESUME_FILE = "UpdatedResumeDataset.csv"
+JOB_FILE = "raw_data_v2.csv"
 EMBEDDING_SIZE = 50
 
 class InputType(Enum):
@@ -12,13 +15,15 @@ class InputType(Enum):
     RESUME = "resume"
     EXTRA = "extracurricular"
 
-def file_to_csv(filename: str):
-    with open(filename, 'r') as f:
+def file_to_csv(filename: str, max_lines=100):
+    with open(filename, 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
         column_names, rows = [], []
         for i, line in enumerate(reader):
             if i == 0:
                 column_names = line
+            elif i > max_lines:
+                break
             else:
                 current = { column_names[j]: text 
                     for j, text in enumerate(line) }
@@ -42,22 +47,37 @@ def create_embedding_dict():
 # 3. Concentate the embeddings
 # 4. Pad if fewer than 5 keywords were found, to ensure consistent length
 def featurize(input: dict, input_type: str, num_keywords=5):
-    if input_type == InputType.EXTRA.value:
-        text = f"{input['Requisitos']}"
-        keywords = spacy_nlp(text).ents
-        vector = np.asarray([], dtype="float32")
-        for word in keywords[:num_keywords]:
-            word_vec = embeddings_dict.get(str(word).lower(), [])
-            if len(word_vec) > 0:
-                vector = np.concatenate((vector, word_vec))
-        padding = [0.0 for i in range(num_keywords * EMBEDDING_SIZE - len(vector))]
-        vector = np.concatenate((vector, padding))
-        return vector
+    relevant_fields = {
+        InputType.EXTRA.value: ['Requisitos', 'Descripcion'],
+        InputType.RESUME.value: ['Resume'],
+        InputType.JOB.value: []
+    }
+    text = " ".join([input[field] for field in relevant_fields[input_type]])
+    keywords = spacy_nlp(text).ents
+    vector = np.asarray([], dtype="float32")
+    for word in keywords[:num_keywords]:
+        word_vec = embeddings_dict.get(str(word).lower(), [])
+        if len(word_vec) > 0:
+            vector = np.concatenate((vector, word_vec))
+    padding = [0.0 for i in range(num_keywords * EMBEDDING_SIZE - len(vector))]
+    vector = np.concatenate((vector, padding))
+    return vector
+
+def create_feature_vectors(filename: str, input_type: str):
+    csv_data = file_to_csv(filename)
+    all_vecs = []
+    for d in csv_data:
+        all_vecs.append(featurize(d, input_type))
+    return all_vecs
 
 if __name__ == "__main__":
     embeddings_dict = create_embedding_dict()
-    extracurriculars = file_to_csv(filename="OYA_AcademicJobs.csv")
-    extra_vectors = []
-    for extra in extracurriculars:
-        extra_vectors.append(featurize(extra, "extracurricular"))
+    params = [(EXTRA_FILE, InputType.EXTRA.value), 
+        (RESUME_FILE, InputType.RESUME.value), (JOB_FILE, InputType.JOB.value)]
+    extracurriculars, resumes, jobs = [create_feature_vectors(filename, input_type)
+        for filename, input_type in params]
+    
+    
+
+    
 
