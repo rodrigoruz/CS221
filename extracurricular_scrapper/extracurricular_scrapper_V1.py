23493642a20a43b5b94c024cb64733bcbad9dd5c
rodrigoruz <rodrigo@climate.ai>
2022-12-04 10:00:03 -0800

    changes to extracurricular scrapper

--- extracurricular_scrapper/extracurricular_scrapper_V1.py
+++ extracurricular_scrapper/extracurricular_scrapper_V1.py
@@ -37,100 +37,7 @@ counter = 0
 rows = []
 
 # All in same directory
-DRIVER_PATH = '/Users/rodrigoruz/CS221/chromedriver.exe'
-
-# Methods for Image Scrapping
-def fetch_image_urls(query: str, max_links_to_fetch: int, wd: webdriver, sleep_between_interactions: int = 3):
-    def scroll_to_end(wd):
-        wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(sleep_between_interactions)
-        # build the google query
-
-    # search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
-    search_url = 'https://www.google.com/search?tbm=isch&q=' + query + "&as_epq=&as_oq=&as_eq=&cr=&as_sitesearch=&safe=images&tbs=isz:m"
-
-    # load the page
-    wd.get(search_url.format(q=query))
-
-    image_urls = set()
-    image_count = 0
-    results_start = 0
-    error_clicks = 0
-    while (image_count < max_links_to_fetch) & (
-            error_clicks < 30):  # error clicks to stop when there are no more results to show by Google Images. You can tune the number
-        scroll_to_end(wd)
-
-        print('Starting search for Images')
-
-        # get all image thumbnail results
-        thumbnail_results = wd.find_elements_by_css_selector("img.Q4LuWd")
-        number_results = len(thumbnail_results)
-
-        print(f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}")
-        for img in thumbnail_results[results_start:max_links_to_fetch]:
-            # try to click every thumbnail such that we can get the real image behind it
-            print("Total Errors till now:", error_clicks)
-            try:
-                print('Trying to Click the Image')
-                img.click()
-                time.sleep(sleep_between_interactions)
-                print('Image Click Successful!')
-            except Exception:
-                error_clicks = error_clicks + 1
-                print('ERROR: Unable to Click the Image')
-                if (results_start < number_results):
-                    continue
-                else:
-                    break
-
-            results_start = results_start + 1
-
-            # extract image urls
-            print('Extracting of Image URLs')
-            actual_images = wd.find_elements_by_css_selector('img.n3VNCb')
-            for actual_image in actual_images:
-                if actual_image.get_attribute('src') and 'http' in actual_image.get_attribute('src'):
-                    image_urls.add(actual_image.get_attribute('src'))
-
-            image_count = len(image_urls)
-
-            print('Current Total Image Count:', image_count)
-
-            if len(image_urls) >= max_links_to_fetch:
-                print(f"Found: {len(image_urls)} image links, done!")
-                break
-            else:
-                image_urls="-"
-                load_more_button = wd.find_element_by_css_selector(".mye4qd")
-                if load_more_button:
-                    wd.execute_script("document.querySelector('.mye4qd').click();")
-
-        results_start = len(thumbnail_results)
-
-    return image_urls
-
-
-def persist_image(folder_path: str, file_name: str, url: str):
-    try:
-        image_content = requests.get(url).content
-
-    except Exception as e:
-        print(f"ERROR - Could not download {url} - {e}")
-
-    try:
-        image_file = io.BytesIO(image_content)
-        image = Image.open(image_file).convert('RGB')
-        folder_path = os.path.join(folder_path, file_name)
-        if os.path.exists(folder_path):
-            file_path = os.path.join(folder_path, hashlib.sha1(image_content).hexdigest()[:10] + '.jpg')
-        else:
-            os.mkdir(folder_path)
-            file_path = os.path.join(folder_path, hashlib.sha1(image_content).hexdigest()[:10] + '.jpg')
-        with open(file_path, 'wb') as f:
-            image.save(f, "JPEG", quality=85)
-        print(f"SUCCESS - saved {url} - as {file_path}")
-    except Exception as e:
-        print(f"ERROR - Could not save {url} - {e}")
+DRIVER_PATH = '/Users/rodrigoruz/CS221/extracurricular_scrapper/chromedriver.exe'
 
 #Sofi
 print("Choose Discipline...1:Anthropology, 2:Archeology, 3:Scenic Arts, 4: Visual Arts, 5:Biology, 6:Aerospace")
@@ -380,24 +287,6 @@ for i in range(1, MAX_PAGES): # Iterate through the number of pages in the websi
                             requisites = split_string[0]
                         except:
                             pass
-                        ## URL Image
-                        # taking user input
-                        print(title)
-                        if __name__ == '__main__':
-                            queryinput = input("Enter an image query: ")
-                            wd = webdriver.Chrome(executable_path=DRIVER_PATH)
-                            queries = [queryinput]  # change your set of queries here
-                            for query in queries:
-                                wd.get('https://google.com')
-                                search_box = wd.find_element_by_css_selector('input.gLFyf')
-                                search_box.send_keys(query)
-                                links = fetch_image_urls(query, 1, wd)  # 1 denotes no. of images you want to download
-                                images_path = 'dataset/'
-                                for i in links:
-                                    persist_image(images_path, query, i)
-                            wd.quit()
-                            selected_url=str(links)
-                            selected_url = selected_url[2:-2]
 
                 print(counter, "-", title, deadline, location, official_link_4)
                 # Generate Dataframe
