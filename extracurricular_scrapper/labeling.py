cd5693bd7f268b57954bf8d5a30a2327986d4054
rodrigoruz <rodrigo@climate.ai>
2022-12-05 08:32:56 -0800

    format changes

--- extracurricular_scrapper/labeling.py
+++ extracurricular_scrapper/labeling.py
@@ -3,8 +3,21 @@ import glob
 import pandas as pd
 from extractor import extract_job_details
 # os.chdir("/..") # if files are located in different directory
+import sys
+
+# setting path
+sys.path.append('../extracurricular_scrapper')
+
+# importing
+from extracurricular_scrapper.extractor import extract_job_details
+
+# using
+geek_method()
+
 
 filename = 'UpdatedResumeDataSet.csv'
 df = pd.read_csv(filename)
 category = df.Category.unique()
 print(category)
+
+# csv_to_dict_list -> gives list of dictionaries input
